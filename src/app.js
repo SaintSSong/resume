49fba74d94ea996d9e1d0e0a32b07a572e54d1e8
songsamuel <songsme2886@gmail.com>
2024-05-27 00:40:34 +0900

    로그인 API 50% 구현 및 스키마 재 설정

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 import express from 'express'; 
 import AuthRouter from './routers/auth.router.js';
+import cookieParser from 'cookie-parser';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 const PORT = 3030;
 
+// 비밀 키는 외부에 노출되면 안되겠죠? 그렇기 때문에, .env 파일을 이용해 비밀 키를 관리해야합니다.
+const ACCESS_TOKEN_SECRET_KEY = `HangHae99`; // Access Token의 비밀 키를 정의합니다.
+const REFRESH_TOKEN_SECRET_KEY = `Sparta`; // Refresh Token의 비밀 키를 정의합니다.
+
 app.use(express.json());
+app.use(cookieParser());
 app.use('/', [AuthRouter]);
 
+
+const tokenStorages = {} // 리프레시 토큰을 관리할 객체
 
 app.listen(PORT,()=>{
     console.log(PORT, "포트로 서버가 열렸습니다.")
--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -5,6 +5,8 @@ import bcrypt from 'bcrypt'; // 비밀번호 hash를 값 때문에 작성
 
 const router = express.Router(); // 라우터 생성!
 
+
+// 회원가입 API
 router.post('/auth/sign-up', async (req, res, next) => {
     //     1.  **이메일, 비밀번호, 비밀번호 확인, 이름**을 **Request Body**(**`req.body`**)로 전달 받습니다.
     const { email, password, passwordCheck, name } = req.body;
@@ -12,22 +14,22 @@ router.post('/auth/sign-up', async (req, res, next) => {
 
     //     - **회원 정보 중 하나라도 빠진 경우** - “OOO을 입력해 주세요.”
     if (!email) {
-        return res.status(400).json({ message: 'email을 입력해 주세요.' });
+        return res.status(400).json({ errorMessage: 'email을 입력해 주세요.' });
     }
     if (!password) {
-        return res.status(400).json({ message: 'password를 입력해 주세요.' });
+        return res.status(400).json({ errorMessage: 'password를 입력해 주세요.' });
     }
     if (!passwordCheck) {
-        return res.status(400).json({ message: 'passwordCheck를 입력해 주세요.' });
+        return res.status(400).json({ errorMessage: 'passwordCheck를 입력해 주세요.' });
     }
     if (!name) {
-        return res.status(400).json({ message: 'name를 입력해 주세요.' });
+        return res.status(400).json({ errorMessage: 'name를 입력해 주세요.' });
     }
 
     //     - **이메일 형식에 맞지 않는 경우** - “이메일 형식이 올바르지 않습니다.”
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;     /** 정규 표현식**/
     if (!emailRegex.test(email)) {
-        return res.status(400).json({ message: '이메일 형식이 올바르지 않습니다.' });
+        return res.status(400).json({ errorMessage: '이메일 형식이 올바르지 않습니다.' });
     }
 
     //     - **이메일이 중복되는 경우** - “이미 가입 된 사용자입니다.”
@@ -36,17 +38,17 @@ router.post('/auth/sign-up', async (req, res, next) => {
         where: { email: email },    // ** where는 왜 쓰는가? 무슨 뜻이지?? 행열이라고 생각하니까 답이 나온다. 즉 스키마프리즈마 run 시키면 나오는 행열에 email이란 열에서 찾기 때문에 where! 어디 열에서 찾을 거냐 이런 뜻이 된다.
     });
     if (isExistUser) {
-        return res.status(409).json({ message: '이미 가입 된 사용자입니다.' });
+        return res.status(409).json({ errorMessage: '이미 가입 된 사용자입니다.' });
     }
 
     //     - **비밀번호가 6자리 미만인 경우** - “비밀번호는 6자리 이상이어야 합니다.”
     if (password.length < 6) {
-        return res.status(400).json({message: '비밀번호는 6자리 이상이어야 합니다.' });
+        return res.status(400).json({ errorMessage: '비밀번호는 6자리 이상이어야 합니다.' });
     }
 
     //     - **비밀번호와 비밀번호 확인이 일치하지 않는 경우** - “입력 한 두 비밀번호가 일치하지 않습니다.”
-    if(password !== passwordCheck) {
-        return res.status(401).json({errorMessage: '입력한 두 비밀번호가 일치하지 않습니다.' });
+    if (password !== passwordCheck) {
+        return res.status(401).json({ errorMessage: '입력한 두 비밀번호가 일치하지 않습니다.' });
     }
 
     //     3. **비즈니스 로직(데이터 처리)**
@@ -76,7 +78,7 @@ router.post('/auth/sign-up', async (req, res, next) => {
     //     4. **반환 정보**
     //     - **사용자 ID, 이메일, 이름, 역할, 생성일시, 수정일시**를 반환합니다.
 
-    return res.status(201).json({ 
+    return res.status(201).json({
         message: '회원가입이 완료되었습니다.',
         data: {
             userId: user.userId,
@@ -84,13 +86,53 @@ router.post('/auth/sign-up', async (req, res, next) => {
             name: userInfo.name,
             role: userInfo.role,
             createdAt: user.createdAt,
-            updatedAt: user.updatedAt, 
+            updatedAt: user.updatedAt,
         }
     });
 });
 
+/** 로그인 API 생성*/
+router.post('/auth/sign-in', async (req, res, next) => {
+
+    // 1. **요청 정보**
+    //     - **이메일, 비밀번호**를 **Request Body**(**`req.body`**)로 전달 받습니다.
+    const { email, password } = req.body;
+
+    // 2. **유효성 검증 및 에러 처리**
+    //     - **로그인 정보 중 하나라도 빠진 경우** - “OOO을 입력해 주세요.”
+    if (!email) {
+        return res.status(401).json({ errorMessage: "이메일을 입력해 주세요." })
+    }
+    if (!pasword) {
+        return res.status(401).json({ errorMessage: "비밀번호를 입력해 주세요." })
+    }
+
+    //     - **이메일 형식에 맞지 않는 경우** - “이메일 형식이 올바르지 않습니다.”
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;     /** 정규 표현식**/
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({ errorMessage: '이메일 형식이 올바르지 않습니다.' });
+    }
+
+    //     - **이메일로 조회되지 않거나 비밀번호가 일치하지 않는 경우** - “인증 정보가 유효하지 않습니다.”
+    const user = await prisma.users.findFirst({ where: { email: email } });
+    if (!user) {
+        return res.status(401).json({ message: '존재하지 않는 이메일 입니다.' });
+    }
+
+
+    if (password !== password) {
+        return res.status(401).json({ errorMessage: '비밀번호가 일치하지 않습니다.' });
+    }
+
+
+    // 3. **비즈니스 로직(데이터 처리)**
+    //     - **AccessToken(Payload**에 **`사용자 ID`**를 포함하고, **유효기한**이 **`12시간`)**을 생성합니다.
 
 
 
+
+    // 4. **반환 정보**
+    //     - **AccessToken**을 반환합니다.
+});
 
 export default router;
